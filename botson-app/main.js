21d2cd264c18cb40a1b333d061a64f2a7a48d87b
Restyled.io <commits@restyled.io>
2021-08-03 17:07:38 +0000

    Restyled by clang-format

--- botson-app/main.js
+++ botson-app/main.js
@@ -1,67 +1,72 @@
 // Import des scripts / modules
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 const isDev = require("electron-is-dev");
 const path = require('path');
 // const rpc = require('./src/utils/rpc');
 
 /**
- * 
+ *
  * @description Permet de crée la première window
- * 
+ *
  * @returns BrowserWindow
  */
-function createMainWindow () {
-    const win = new BrowserWindow({
-        width: 1200,
-        height: 700,
-        show: false, 
-        icon: __dirname + '/src/img/ico/logo.ico',
-        webPreferences: {
-            preload: 'preload.js',
-        }
-    });
-
-    // rpc.changeRPC({"state": "Sélectionne son bot"})
-
-    win.loadURL(
-        isDev
-          ? "http://localhost:3000"
-          : `file://${path.join(__dirname, "/build/index.html")}`
-    );
-
-    // Ouverture de la DevTools
-    if (isDev) {
-        win.webContents.openDevTools({ mode: "detach" });
+function createMainWindow() {
+  const win = new BrowserWindow({
+    width : 1200,
+    height : 700,
+    show : false,
+    icon : __dirname + '/src/img/ico/logo.ico',
+    webPreferences : {
+      preload : 'preload.js',
     }
+  });
 
-    return win;
+  // rpc.changeRPC({"state": "Sélectionne son bot"})
+
+  win.loadURL(isDev ? "http://localhost:3000"
+                    : `file://${path.join(__dirname, "/build/index.html")}`);
+
+  // Ouverture de la DevTools
+  if (isDev) {
+    win.webContents.openDevTools({mode : "detach"});
+  }
+
+  return win;
 }
 
 //////////////////////////////////////////////
 app.on('ready', () => {
 
     let main = null
-    // Création d'une fenetre de chargement
-    let loading = new BrowserWindow({show: false, frame: false, width: 300, height: 400, icon: __dirname + '/src/img/ico/logo.ico',})
-  
-    loading.loadFile('./build/loading.html')
+// Création d'une fenetre de chargement
+let loading = new BrowserWindow({
+  show : false,
+  frame : false,
+  width : 300,
+  height : 400,
+  icon : __dirname + '/src/img/ico/logo.ico',
+})
+
+loading.loadFile('./build/loading.html')
 
     loading.once('show', () => {
         main = createMainWindow()
         main.webContents.once('dom-ready', () => {
             console.log('main loaded')
-            main.show()
-            loading.hide()
+        main.show()
+        loading.hide()
             loading.close()
         })
     })
-    loading.show()
+            loading.show()
 
     app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+  if (BrowserWindow.getAllWindows().length === 0)
+    createMainWindow()
     })
 });
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+    app.on('window-all-closed', function() {
+      if (process.platform !== 'darwin')
+        app.quit()
+    })
--- botson-app/src/App.js
+++ botson-app/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
+  Switch,
 } from 'react-router-dom';
 
 export default class App extends React.Component {
