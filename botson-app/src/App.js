4d94587e2a673c23a1b7329b74a1fae8cc34cd05
Nirbose <nirbose2@gmail.com>
2021-08-03 18:48:56 +0200

    Components
    Ajout de fichier components

--- botson-app/src/App.js
+++ botson-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <React.StrictMode>
+        <Router>
+          <Switch>
+            <Route path="/home" exact>
+              <h1>Test</h1>
+            </Route>
+            <Route path="/settings">
+              <h1>Test2</h1>
+            </Route>
+            <Route path="/" exact>
+              <h1>Test3</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </React.StrictMode>
+    );
+  }
 }
-
-export default App;
---
+++ botson-app/src/components/LoginCard.jsx
@@ -0,0 +1,16 @@
+export function LoginCard() {
+    return(
+        <div className="login-card">
+
+            <h2>BotsOn</h2>
+
+            <form className="login-form">
+                <button type="submit" className="btn btn-login">Connexion</button>
+            </form>
+
+            <div className="login-links">
+                
+            </div>
+        </div>
+    );
+}
